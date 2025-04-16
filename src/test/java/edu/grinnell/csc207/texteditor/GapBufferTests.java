aa4e7de9b1899d79f8396f318ebe1f1281c92f6f
tangyixu <tangyixu@grinnell.edu>
2025-04-15 23:25:23 -0500

    fixed tests

--- src/test/java/edu/grinnell/csc207/texteditor/GapBufferTests.java
+++ src/test/java/edu/grinnell/csc207/texteditor/GapBufferTests.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 public class GapBufferTests {
-    
+      
     @Test
     public void normalInsertTest() {
         GapBuffer sample = new GapBuffer();
